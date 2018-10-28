eea6dd76ecd7bf6b6b3df61c3cf3cfd805484aa2
Andrew Eissen <aeissen@gmail.com>
2018-10-28 10:46:38 -0400

    First commit

---
+++ Files/js/app.js
@@ -0,0 +1,67 @@
+/**
+ * @file app.js
+ * @fileoverview The main module of the program, contains several access
+ * namespaces denoting which functions, arrays, enums, and variables may be
+ * returned for external or global usage. Begun 10/28/18
+ * @author Andrew Eissen
+ */
+'use strict';
+
+/**
+ * @description The primary JavaScript module.
+ *
+ * @see {@link //google.github.io/styleguide/jsguide.html|Styleguide #1}
+ * @see {@link //google.github.io/styleguide/javascriptguide.xml|Styleguide #2}
+ * @author Andrew Eissen
+ * @module BookkeepingProjectModule
+ * @const
+ */
+const BookkeepingProjectModule = (function () {
+
+  // Declare access object namespaces (basically public and private)
+  let accessible, inaccessible;
+
+  // Define access object namespaces
+  accessible = accessible || {};
+  inaccessible = inaccessible || {};
+
+  /**
+   * @description This constant is used to test the program and display messages
+   * in the console. Though not a part of the <code>inaccessible</code> object,
+   * it is still contained within the private restricted scope of the
+   * <code>BookkeepingProjectModule</code> IIFE and cannot be accessed
+   * externally.
+   *
+   * @const
+   */
+  const DEBUG = false;
+
+  /**
+   * @description This function serves as the main initialization function,
+   * called on the completion of the DOM load by the externally-facing function
+   * <code>accessible.init</code>. Ideally, it will dynamically generate HTML
+   * via some internal helper functions and fade in on the scene via the use of
+   * a <code>jQuery</code>-esque fade-in function.
+   *
+   * @returns {void}
+   */
+  inaccessible.main = function () {
+    console.log('Testing');
+  };
+
+  /**
+   * @description The central function of the <code>accessible</code> access
+   * scope namespace, <code>init</code> is called on completion of the loading
+   * of the HTML <code>body</code> element. This method is presently the only
+   * externally accessible function or variable of the module, and simply calls
+   * <code>inaccessible.main</code> to get the program started.
+   *
+   * @returns {void}
+   */
+  accessible.init = function () {
+    inaccessible.main();
+  };
+
+  // Return globally-accessible object
+  return accessible;
+})();
---
+++ README.md
@@ -0,0 +1,21 @@
+### CMSC 495 Bookkeeping Project ###
+
+#### Overview ####
+
+This project, the central focus of the UMUC capstone class CMSC 495 Current Trends and Projects in Computer Science, is an implementation of a simple bookkeeping system powered by a variety of different technologies and languages, ranging from PHP to JavaScript.
+
+#### Participants ####
+
+* Matthew Dobson
+* Kevin Kurt Ramirez
+* Christian Rondon
+* Jennifer Brady
+* Andrew Eissen [(@andreweissen)](https://github.com/andreweissen)
+* Steven Wu
+
+#### Resources ####
+
+* [Udacity's "How to Use Git and GitHub" free course](https://www.udacity.com/course/how-to-use-git-and-github--ud775)
+* [GitHub markdown syntax cheatsheet](https://github.com/tchapi/markdown-cheatsheet/blob/master/README.md)
+* [JS Google Styleguide #1](https://google.github.io/styleguide/jsguide.html)
+* [JS Google Styleguide #2](https://google.github.io/styleguide/javascriptguide.xml)
