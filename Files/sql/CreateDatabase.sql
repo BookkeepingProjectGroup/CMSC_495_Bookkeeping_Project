0081a9dc0688f220a06df9fbc9be15763c666bc5
dobs93 <40001263+dobs93@users.noreply.github.com>
2018-11-01 16:21:48 -0400

    Add a Users table; modify the Accounts table
    The Users table and userID fields in Documents and Accounts are meant to allow multiple users' data to be stored in the database while keeping it strictly separate.

--- Files/sql/CreateDatabase.sql
+++ Files/sql/CreateDatabase.sql
@@ -1,35 +1,51 @@
+CREATE TABLE Users(
+    ID     INT         UNSIGNED NOT NULL,
+    name   VARCHAR(64),
+    PRIMARY KEY (userID)
+);
+
 CREATE TABLE Documents(
-    documentID INT                                UNSIGNED NOT NULL,
+    ID         INT                                UNSIGNED NOT NULL,
+    userID     INT                                UNSIGNED NOT NULL,
     vendorID   INT                                UNSIGNED,
     customerID INT                                UNSIGNED,
     name       VARCHAR(16),
     type       ENUM('JE','API','APD','ARI','ARR')          NOT NULL,
     isPosted   TINYINT(1)                         UNSIGNED NOT NULL,
-    PRIMARY KEY (documentID),
-    FOREIGN KEY (vendorID)   REFERENCES Vendors(vendorID),
-    FOREIGN KEY (CustomerID) REFERENCES Customers(customerID),
+    PRIMARY KEY (ID),
+    FOREIGN KEY (userID)     REFERENCES Users(ID),
+    FOREIGN KEY (vendorID)   REFERENCES Vendors(ID),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(ID),
     CHECK (vendorID IS NULL) OR (customerID IS NULL),
     CHECK (isPosted = 0) OR (isPosted = 1)
 );
 
 CREATE TABLE Accounts(
-    accountID SMALLINT(4) UNSIGNED NOT NULL,
+    ID        INT             UNSIGNED NOT NULL,
+    userID    INT             UNSIGNED NOT NULL,
+    code      SMALLINT(4)     UNSIGNED NOT NULL,
     name      VARCHAR(32),
-    type      TINYINT(1)  UNSIGNED NOT NULL,
-    PRIMARY KEY (accountID)
+    type      ENUM(
+                  'ASSET',
+                  'LIABILITY',
+                  'EQUITY',
+                  'REVENUE',
+                  'EXPENSE')  UNSIGNED NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (userID) REFERENCES Users(ID)
 );
 
 CREATE TABLE GeneralLedger(
-    lineID      INT           UNSIGNED NOT NULL,
+    ID          INT           UNSIGNED NOT NULL,
     documentID  INT           UNSIGNED NOT NULL,
-    accountID   SMALLINT(4)   UNSIGNED NOT NULL,
+    accountID   INT           UNSIGNED NOT NULL,
     lineDate    DATE                   NOT NULL,
     debit       DECIMAL(10,2),
     credit      DECIMAL(10,2),
     description VARCHAR(64),
-    PRIMARY KEY (lineID),
-    FOREIGN KEY (documentID) REFERENCES Documents(documentID),
-    FOREIGN KEY (accountID)  REFERENCES Accounts(accountID),
+    PRIMARY KEY (ID),
+    FOREIGN KEY (documentID) REFERENCES Documents(ID),
+    FOREIGN KEY (accountID)  REFERENCES Accounts(ID),
     CHECK
         ((debit IS NULL) AND (credit IS NOT NULL))
         OR ((credit IS NULL) AND (debit IS NOT NULL))
