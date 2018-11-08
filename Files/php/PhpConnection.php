8f0e5c88952f837d39819760b062abb3334ae0b7
dobs93 <40001263+dobs93@users.noreply.github.com>
2018-11-08 16:40:55 -0500

    Create PhpConnection.php
    A concrete class to access the database as user php.

---
+++ Files/php/PhpConnection.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * File: PhpConnection.php
+ * Author(s): Matthew Dobson
+ * Date modified: 11-8-2018
+ *
+ * Description: Defines a concrete PHP class extending abstract class
+ * DatabaseConnection to represent, manipulate and transmit a connection to the
+ * MariaDB SQL database through user php.
+ */
+
+include 'DatabaseConnection.php';
+
+/** A class representing a connection MariaDB SQL database as user php. */
+class PhpConnection extends DatabaseConnection {
+    /** The username of user php. */
+    const USER_PHP_USERNAME = 'php';
+
+    /** The credentials file for user php. */
+    const USER_PHP_CREDENTIALS_FILENAME = '../json/phpLogon.json';
+
+    /**
+     * The sole constructor to be used for this subclass.
+     */
+    public function __construct() {
+        parent::__construct(
+            self::USER_PHP_USERNAME, self::USER_PHP_CREDENTIALS_FILENAME);
+    }
+}
