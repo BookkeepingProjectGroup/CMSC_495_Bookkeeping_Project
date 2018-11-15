ce199d46712fa94bdbb5d73372331d5edbb0eff6
dobs93 <40001263+dobs93@users.noreply.github.com>
2018-11-15 17:38:02 -0500

    Update PasswordsConnection.php

--- Files/php/PasswordsConnection.php
+++ Files/php/PasswordsConnection.php
@@ -3,14 +3,14 @@
 /*
  * File: PasswordsConnection.php
  * Author(s): Matthew Dobson
- * Date modified: 11-8-2018
+ * Date modified: 11-15-2018
  *
  * Description: Defines a concrete PHP class extending abstract class
  * DatabaseConnection to represent, manipulate and transmit a connection to the
  * MariaDB SQL database through user passwords.
  */
 
-include 'DatabaseConnection.php';
+include_once 'DatabaseConnection.php';
 
 /** A class representing a connection MariaDB SQL database as user passwords. */
 class PasswordsConnection extends DatabaseConnection {
@@ -27,5 +27,99 @@ class PasswordsConnection extends DatabaseConnection {
         parent::__construct(
             self::USER_PASSWORDS_USERNAME,
             self::USER_PASSWORDS_CREDENTIALS_FILENAME);
+    }
+
+    /**
+     * A method to set or reset a user's password. The password is salted and
+     * hashed using PHP's password_hash() method. If the user already has a
+     * password in the database, the user's password will be changed.
+     *
+     * @param $username the username of the user whose password is to be set or
+     * changed.
+     * @param $password the new password.
+     *
+     * @return TRUE if everything went correctly; FALSE if no user with username
+     * $username exists; 0 if the password was not changed.
+     */
+    public function setUserPassword(string $username, string $password) {
+        // Query UsersDB.Users to get the ID associated with $username.
+        $userID = $this->getUserID($username);
+
+        // If the above returned FALSE, no user with username $username exists,
+        // so return FALSE.
+        if(!$userID) {
+            return FALSE;
+        }
+
+        // Salt and has the password.
+        $saltedAndHashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        // Query to see if the user has a password on record.
+        $getPasswordResult =
+            $this->runQuery(
+                'SELECT saltedAndHashedPassword '
+                    . 'FROM PasswordsDB.Passwords '
+                    . 'WHERE userID = ?',
+                's',
+                $userID);
+
+        // If the above returns FALSE, an error occurred; throw an exception.
+        if($getPasswordResult === FALSE) {
+            throw new DatabaseException(
+                'DatabaseConnection::runQuery(string,string,...mixed) failed.');
+        }
+
+        // Branch depending on the number of items resulting from the query.
+        if(count($getPasswordResult) == 0) {
+            // If there is no password set for the user, user INSERT to add the
+            // user and the password to the database.
+            $this->runQuery(
+                'INSERT INTO PasswordsDB.Passwords '
+                    . '(userID, saltedAndHashedPassword) '
+                    . 'VALUES (?, ?)',
+                'ss',
+                $userID,
+                $saltedAndHashedPassword);
+        } elseif(count($getPasswordResult) == 1) {
+            // If there is a password on file for the user, use UPDATE to update
+            // it to the new password.
+            $this->runQuery(
+                'UPDATE PasswordsDB.Passwords '
+                    . 'SET saltedAndHashedPassword = ? '
+                    . 'WHERE userID = ?',
+                'ss',
+                $saltedAndHashedPassword,
+                $userID);
+        } else {
+            // If the query produced more than one result, it appears the user
+            // has more than one password, and this should be an illegal
+            // situation; throw an exception.
+            throw new DatabaseException(
+                'It appears multiple passwords exist for the user provided.');
+        }
+
+        // In order to ensure the password was successfully changed, query the
+        // database for the new password.
+        $getUpdatedPasswordResult =
+            $this->runQuery(
+                'SELECT saltedAndHashedPassword '
+                    . 'FROM PasswordsDB.Passwords '
+                    . 'WHERE userID = ?',
+                's',
+                $userID);
+
+        // If the above returns FALSE, an error occurred; throw an exception.
+        if($getPasswordResult === FALSE) {
+            throw new DatabaseException(
+                'DatabaseConnection::runQuery(string,string,...mixed) failed.');
+        }
+
+        // If the queried password does not equal what it should, return 0.
+        if($getUpdatedPasswordResult[0][0] != $saltedAndHashedPassword) {
+            return 0;
+        }
+
+        // If we made it here, everything must have gone well, so return TRUE.
+        return TRUE;
     }
 }
