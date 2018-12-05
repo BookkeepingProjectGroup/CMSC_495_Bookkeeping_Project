9d875ad04448acb5b3d97d3398e09fde2d481817
dobs93 <dobson.m94@gmail.com>
2018-12-04 22:03:01 -0500

    PHP: Add end-point create_user.php. Add class UsersConnection. Simplify class DatabaseException. Add class DuplicateException. SQL: Add user "users", which has access to add new database users. JSON: Add credentials file usersLogon.json

---
+++ Files/php/UsersConnection.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * File: UsersConnection.php
+ * Author(s): Matthew Dobson
+ * Date modified: 2018-12-04
+ *
+ * Description: Defines a concrete PHP class extending abstract class
+ * DatabaseConnection to represent, manipulate and transmit a connection to the
+ * MariaDB SQL database through user users.
+ */
+
+include_once 'DatabaseConnection.php';
+
+/** A class representing a connection MariaDB SQL database as user users. */
+class UsersConnection extends DatabaseConnection {
+    /** The username of user users. */
+    const USER_USERS_USERNAME = 'users';
+
+    /** The credentials file for user passwords. */
+    const USER_USERS_CREDENTIALS_FILENAME = '../json/usersLogon.json';
+
+    /**
+     * The sole constructor to be used for this subclass.
+     */
+    public function __construct() {
+        parent::__construct(
+            self::USER_USERS_USERNAME,
+            self::USER_USERS_CREDENTIALS_FILENAME);
+    }
+
+    /**
+     * A method to add a user account.
+     *
+     * @param $username the username of the new account.
+     *
+     * @return the userID of the new user; NULL if the username already exists;
+     * FALSE if some other error occurred.
+     */
+    public function addUser(string $username) {
+        // If the new username is less than one character in length it is
+        // invalid, so return FALSE.
+        if(strlen($username) < 1) return FALSE;
+
+        // Query the database for the given username; if that does not return
+        // FALSE, a user already exists with that username, so return NULL.
+        if(!$this->getUserID($username)) return NULL;
+
+        // Insert the new user into the database.
+        $this->runQuery(
+            'INSERT INTO BooksDB.Users (username) VALUES (?)',
+            's',
+            $username);
+
+        // Return the new user's userID.
+        return $this->getUserID($username);
+    }
+}
+
