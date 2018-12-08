070ecc07f365921e4d6895952f77164883975316
dobs93 <dobson.m94@gmail.com>
2018-12-08 16:30:11 -0500

    PHP: Complete end-point create_default_accounts.php.

--- Files/php/create_default_accounts.php
+++ Files/php/create_default_accounts.php
@@ -30,42 +30,69 @@
 
 require_once('PhpConnection.php');
 
+// Start the session.
 session_start();
 
+// Initialize the JSON reply object.
 $replyObject = array('success' => FALSE);
 
 if(!array_key_exists('userID', $_SESSION)) {
+    // If "userID" is not set for this session ...
+
+    // Add parameter "userLoggedIn" to the reply object, and set it to FALSE.
     $replyObject['userLoggedIn'] = FALSE;
 
+    // Yield the JSON reply object.
     echo json_encode($replyObject);
 
+    // Exit.
     exit();
 }
 
 try {
+    // Connect to the database through user "php".
     $phpConnection = new PhpConnection();
 
+    // Add the default accounts for the session user; store the returned list of
+    // accounts added into parameter "accountsAdded" in the JSON reply object.
     $replyObject['accountsAdded'] = $phpConnection->addDefaultAccounts(
         $_SESSION['userID']
     );
 
+    // Close the database connection, and unset its token.
     $phpConnection->close();
-
     unset($phpConnection);
 
+    // Set the JSON reply object's "success" parameter to TRUE.
+    $replyObject['success'] = TRUE;
+
+    // Yield the JSON reply object.
     echo json_encode($replyObject);
 } catch(Throwable $e) {
+    // If any exception or error is thrown above ...
+
     if(isset($replyObject['accountsAdded'])) {
+        // If the JSON reply object's "accountsAdded" parameter became set ...
+
+        // Unset it.
         unset($replyObject['accountsAdded']);
     }
 
+    // Set the JSON reply object's "userLoggedIn" parameter to TRUE.
     $replyObject['userLoggedIn'] = TRUE;
 
+    // Yield the JSON reply object.
     echo json_encode($replyObject);
 
+    // Exit.
     exit();
 } finally {
+    // If an exception is thrown in the above ...
+
     if(isset($phpConnection)) {
+        // If $phpConnection is set ...
+
+        // Unset it.
         $phpConnection->close();
     }
 }
